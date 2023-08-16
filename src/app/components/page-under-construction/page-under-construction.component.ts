8bdea5ce255bff00622c4fcfac8a54873bb6293e
sergiosessanta9 <sergioduca639@gmail.com>
2023-08-16 17:57:27 +0200

    Fourth commit.

---
+++ src/app/components/page-under-construction/page-under-construction.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'page-under-construction',
+  templateUrl: './page-under-construction.component.html',
+  styleUrls: ['./page-under-construction.component.scss']
+})
+export class PageUnderConstructionComponent {
+}
