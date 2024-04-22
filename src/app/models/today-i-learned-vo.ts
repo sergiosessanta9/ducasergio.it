678ee22045746b065bd2f5165a7050811a9b5e28
sergiosessanta9 <sergioduca639@gmail.com>
2024-04-22 16:23:25 +0200

    Fix codes for markdown

--- src/app/models/today-i-learned-vo.ts
+++ src/app/models/today-i-learned-vo.ts
@@ -1,11 +1,13 @@
 export class TodayILearnedVO {
     markdown?: string;
     creation_date?: string;
+    format_date?: string;
     url?: string;
 
-    constructor(markdown: string, creation_date: string, url: string) {
+    constructor(markdown: string, creation_date: string, format_date: string, url: string) {
         this.markdown = atob(markdown);
         this.creation_date = creation_date;
+        this.format_date = format_date;
         this.url = url;
     }
     
